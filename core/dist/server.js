7e0f45be976cbf744b17d34ed4cf87ebc43ce341
Maxime Soulard <max.soulard@gmail.com>
2017-06-25 17:37:23 +0200

    GET sensors objects and data.

--- core/dist/server.js
+++ core/dist/server.js
@@ -3,8 +3,9 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var bodyParser = require("body-parser");
 var express = require("express");
+var Promise = require("bluebird");
+var mongodb = require("mongodb");
 var sensors_1 = require("./sensors");
-var users_1 = require("./users");
 var Server = (function () {
     function Server() {
         this.configure();
@@ -14,14 +15,21 @@ var Server = (function () {
         return new Server();
     };
     Server.prototype.configure = function () {
+        var _this = this;
         this.app = express();
         this.app.use(bodyParser.urlencoded({ extended: false }));
         this.app.use(bodyParser.json());
-        this.app.listen('8888');
+        mongodb.MongoClient.connect("mongodb://192.168.0.17/pleasedont-die", { promiseLibrary: Promise })
+            .catch(function (err) { return console.error(err.stack); })
+            .then(function (db) {
+            _this.app.locals.db = db;
+            _this.app.listen("8888", function () {
+                console.log("Node.js app is listening at http://localhost:8888");
+            });
+        });
     };
     Server.prototype.routes = function () {
         var sensors = new sensors_1.Sensors(this.app);
-        var users = new users_1.Users(this.app);
     };
     return Server;
 }());
--- core/src/sensors.ts
+++ core/src/sensors.ts
@@ -1,32 +1,65 @@
 import express = require('express');
-import fs = require('fs');
+import mongodb = require('mongodb');
 
 export class Sensors {
     private app: express.Application;
+    private db: mongodb.Db;
 
     constructor(app: express.Application) {
         this.app = app;
-        this.routes();
+        this.routes();      
     }
 
     private routes() {
         this.app.get('/sensors', this.getAllSensors);
         this.app.get('/sensors/:id', this.getOneSensor);
+        this.app.get('/sensors/:id/data', this.getData);
     }
 
-    private getAllSensors(req: express.Request, res: express.Response): any {
-        res.send('todo all sensors');
+    private getAllSensors(req: express.Request, res: express.Response): void {
+        req.app.locals.db.collection('sensors').find().toArray((err: any, sensors: Sensor[]) => {
+                if (sensors)     res.send(sensors);
+                else            res.sendStatus(404);
+        });
     }
 
-    private getOneSensor(req: express.Request, res: express.Response): any {
-        fs.createReadStream('python/sensor_'+req.params.id+'.json')
-            .on('error', (err: any) => {
-                const codeStatus = err.code === 'ENOENT' ? 404 : 500;
-                res.status(codeStatus).end();
-            })
-            .on('end', () => {
-                res.end();
-            })
-            .pipe(res);
+    private getOneSensor(req: express.Request, res: express.Response): void {
+        const query = { _id: req.params.id };
+
+        req.app.locals.db.collection('sensors').findOne(query)
+            .then((sensor: Sensor) => {
+                if (sensor)     res.send(sensor);
+                else            res.sendStatus(404);
+            });
     }
+
+    private getData(req: express.Request, res: express.Response): void {
+        let options = {"sort": [['_id','desc']]}
+        const query = { sensorid: req.params.id };
+
+        req.app.locals.db.collection('data').findOne(query, options)
+            .then((sensor: SensorData) => {
+                if (sensor)     res.send(sensor);
+                else            res.sendStatus(404);
+            });
+    }
+}
+
+interface Sensor {
+    _id: String;
+    type: String;
+    date: Date;
+    subscribers: Subscriber[];
+}
+
+interface Subscriber {
+    mail: String;
+}
+
+interface SensorData {
+    _id: mongodb.ObjectID;
+    date: Date;
+    hum: String;
+    temp: String;
+    sensorid: String;
 }
