5194b085c10b2a344ac5145ca14e19d941a6bb16
Maxime Soulard <max.soulard@gmail.com>
2017-06-25 19:11:39 +0200

    Sensors mac addresses are hardcoded only here - FIXME

--- core/python/SensorsBatch.py
+++ core/python/SensorsBatch.py
@@ -6,7 +6,7 @@ if __name__ == '__main__':
     LOCK = threading.Lock()
     BT_TASKS = []
     BT_TASKS.append(SensorTask(LOCK, "98:D3:31:70:68:51"))
-    #BT_TASKS.append(SensorTask(LOCK, "00:14:03:05:8F:21"))
+    BT_TASKS.append(SensorTask(LOCK, "00:14:03:05:8F:21"))
 
     for task in BT_TASKS:
         task.execute()
