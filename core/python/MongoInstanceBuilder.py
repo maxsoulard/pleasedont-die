004d9958bbce3e8ed6ba4f71af1e445beccb154c
maxsoulard <max.soulard@gmail.com>
2017-11-17 14:18:17 +0000

    Host to docker container localhost

--- core/python/MongoInstanceBuilder.py
+++ core/python/MongoInstanceBuilder.py
@@ -3,7 +3,7 @@ from pymongo import MongoClient
 
 class MongoInstanceBuilder:
     def __init__(self):
-        self.client = MongoClient()
+        self.client = MongoClient('172.17.0.1')
         self.db = self.client['pleasedont-die']
 
     def get_db(self):
